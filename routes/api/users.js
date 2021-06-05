ead2bb614cab7886130126359b28bb4c9eabd1b0
Scott Alton <scottaltonmusic@gmail.com>
2021-06-04 20:49:34 -0400

    Added express validation for user registration post.

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
-// @route GET api/users public
-router.get('/', (req, res) => res.send('User route'));
+// @route POST api/users public
+router.post(
+  '/',
+  [
+    check('name', 'Name is required.').not().isEmpty(),
+    check('email', 'Please include a valid email address.').isEmail(),
+    check('password', 'Please enter a password that is at least 8 characters in length.').isLength({ min: 8 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send('User route');
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 // Connect to database
 connect();
 
+// Middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API up and running!'));
 
 // Define routes
