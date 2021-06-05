b8283da5e0266fa6688ba5fd8a743a901996a6ec
Scott Alton <scottaltonmusic@gmail.com>
2021-06-05 18:49:24 -0400

    Created profile schema.

---
+++ models/Profile.js
@@ -0,0 +1,75 @@
+const mongoose = require('mongoose');
+
+const ProfileSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+  },
+  instrument: {
+    type: String,
+  },
+  level: {
+    type: String,
+  },
+  location: {
+    type: String,
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+  bio: {
+    type: String,
+  },
+  genres: {
+    type: [String],
+  },
+  years: {
+    type: Number,
+  },
+  education: [
+    {
+      school: {
+        type: String,
+      },
+      credential: {
+        type: String,
+      },
+      field: {
+        type: String,
+      },
+      from: {
+        type: Date,
+      },
+      to: {
+        type: Date,
+      },
+      description: {
+        type: String,
+      },
+    },
+  ],
+  social: {
+    youtube: {
+      type: String,
+    },
+    twitter: {
+      type: String,
+    },
+    facebook: {
+      type: String,
+    },
+    linkedin: {
+      type: String,
+    },
+    instagram: {
+      type: String,
+    },
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = Profile = mongoose.model('profile', profileSchema);
