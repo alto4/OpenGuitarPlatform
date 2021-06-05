5e4d638307e65b026e3e1b096ff722e76d9b4ebb
Scott Alton <scottaltonmusic@gmail.com>
2021-06-05 17:44:22 -0400

    Added user auth route with JWT middleware.

---
+++ middleware/auth.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+module.exports = function (req, res, next) {
+  // Get JWT from request header
+  const token = req.header('x-auth-token');
+
+  // Check for token
+  if (!token) {
+    // 401 -> access denied
+    return res.status(401).json({ msg: 'No access token could be generated. Access denied' });
+  }
+
+  // Verify token
+  try {
+    const decoded = jwt.verify(token, config.get('jwtSecret'));
+
+    req.user = decoded.user;
+    next();
+  } catch (error) {
+    res.status(401).json({ msg: 'Invalid access token.' });
+  }
+};
