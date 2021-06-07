827a7c677a7f041950276d01a6141c397d03256f
Scott Alton <scottaltonmusic@gmail.com>
2021-06-07 13:20:11 -0400

    Added comment routes to lessons and posts.

--- routes/api/lessons.js
+++ routes/api/lessons.js
@@ -87,7 +87,7 @@ router.delete('/:id', auth, async (req, res) => {
       return res.status(404).json({ message: 'Lesson not found.' });
     }
 
-    // Verify that logged in user is the creator of the post
+    // Verify that logged in user is the creator of the lesson
     if (lesson.user.toString() !== req.user.id) {
       return res.status(401).json({ message: 'User not authorized to delete this lesson.' });
     }
@@ -126,7 +126,7 @@ router.put('/enroll/:id', auth, async (req, res) => {
   }
 });
 
-// @route PUT api/posts/unenroll/:id private - unenroll from a course
+// @route PUT api/lessons/unenroll/:id private - unenroll from a course
 router.put('/unenroll/:id', auth, async (req, res) => {
   try {
     const lesson = await Lesson.findById(req.params.id);
@@ -146,6 +146,75 @@ router.put('/unenroll/:id', auth, async (req, res) => {
   } catch (error) {
     console.error(error.message);
     res.status(500).send('Server Error');
+  }
+});
+
+// @route POST api/lessons/comment/:id private - comment on an existing lesson
+router.post(
+  '/comment/:id',
+  [auth, [check('text', 'Text is required to create a new comment.').not().isEmpty()]],
+
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+
+      const lesson = await Lesson.findById(req.params.id);
+
+      const newComment = {
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      };
+
+      console.log(lesson);
+      lesson.comments.unshift(newComment);
+
+      await lesson.save();
+
+      res.json(lesson.comments);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send('Server error.');
+    }
+  }
+);
+
+// @route DELETE api/lessons/comment/:id/:comment_id private - delete a comment
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+  try {
+    const lesson = await Lesson.findById(req.params.id);
+
+    // Grab comment from specified lesson
+    const comment = lesson.comments.find((comment) => comment.id === req.params.comment_id);
+
+    // Check comment exists
+    if (!comment) {
+      return res.status(404).json({ message: 'Comment does not exist.' });
+    }
+
+    // Check user
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ message: 'User not authorized to remove this comment.' });
+    }
+
+    // Get removal index
+    const removeIndex = lesson.comments.map((comments) => comment.user.toString()).indexOf(req.user.id);
+
+    lesson.comments.splice(removeIndex, 1);
+
+    await lesson.save();
+
+    res.json(lesson.comments);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server error.');
   }
 });
 
