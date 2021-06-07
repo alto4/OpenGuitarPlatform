70c495d9eb944ba54d42b23e8a036e10ae0de634
Scott Alton <scottaltonmusic@gmail.com>
2021-06-06 20:42:01 -0400

    Added get all and single posts routes.

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -37,4 +37,36 @@ router.post(
   }
 );
 
+// @route GET api/posts private - get all posts
+router.get('/', auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server error.');
+  }
+});
+
+// @route GET api/posts/:id private - get single post
+router.get('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ message: 'Post not found.' });
+    }
+
+    res.json(post);
+  } catch (error) {
+    console.error(error.message);
+
+    if (err.kind === 'ObjectId') {
+      return res.status(404).json({ message: 'Post not found.' });
+    }
+
+    res.status(500).send('Server error.');
+  }
+});
+
 module.exports = router;
