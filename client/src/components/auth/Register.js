a247fe1423e9071237a6d11eb567bc071eb31d7d
Scott Alton <scottaltonmusic@gmail.com>
2021-06-10 19:35:10 -0400

    onChange and onSubmit added to register page.

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -10,6 +10,19 @@ const Register = () => {
 
   const { name, email, password, passwordConfirm } = formData;
 
+  const onChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (password !== passwordConfirm) {
+      console.log('Passwords do not match');
+    } else {
+      console.log(formData);
+    }
+  };
+
   return (
     <Fragment>
       <h1>Register</h1>
@@ -17,19 +30,41 @@ const Register = () => {
         <i className='fa fa-user'></i> Create Your Account
       </p>
 
-      <form className='form'>
+      <form className='form' onSubmit={(e) => onSubmit(e)}>
         <div className='form-group'>
-          <input type='text' placeholder='Name' name='name' required />
+          <input type='text' placeholder='Name' name='name' value={name} onChange={(e) => onChange(e)} required />
         </div>
         <div className='form-group'>
-          <input type='text' placeholder='Email Address' name='email' required />
+          <input
+            type='text'
+            placeholder='Email Address'
+            name='email'
+            value={email}
+            onChange={(e) => onChange(e)}
+            required
+          />
         </div>
         <div className='form-group'>
-          <input type='password' placeholder='Password' name='password' required />
+          <input
+            type='password'
+            placeholder='Password'
+            name='password'
+            value={password}
+            onChange={(e) => onChange(e)}
+            required
+          />
         </div>
         <div className='form-group'>
-          <input type='password' placeholder='Password Confirm' name='passwordConfirm' required />
+          <input
+            type='password'
+            placeholder='Password Confirm'
+            name='passwordConfirm'
+            value={passwordConfirm}
+            onChange={(e) => onChange(e)}
+            required
+          />
         </div>
+        <button className='btn btn-submit'>Register</button>
       </form>
     </Fragment>
   );
